f7a6c5d3538ebd7f04edeb26f6ee2457b054cd24
martin <martin.fillon@epitech.eu>
2023-02-16 14:41:06 +0100

    [ADD] more tests to minishell

--- scripts/minishell.py
+++ scripts/minishell.py
@@ -1,9 +1,10 @@
 import subprocess
 
 errorCode = 84
-segfaultMessage = b"[Segmentation fault]\n\x00"
+segfaultMessage = b"Segmentation fault\n"
 commandNotFound = b"["
 noErr = b""
+noOut = b""
 successCode = 0
 testCount = 0
 
@@ -12,31 +13,44 @@ def TEST(command, sucess, errOut, stdOut):
     global testCount
     testCount += 1
     if errOut == b"[":
-        errOut += bytes(command, 'utf-8')
-        errOut += b": Command not found.]\n\x00"
+        errOut = bytes(command, 'utf-8')
+        errOut += b": Command not found.\n"
     print("Test " + str(testCount) + ' => echo ' + command)
-    command = subprocess.Popen(['echo', command], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    sh = subprocess.Popen(['./mysh'], stdin=command.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    command.wait()
+    cm = subprocess.Popen(['echo', command], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    sh = subprocess.Popen(['./mysh'], stdin=cm.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    cm.wait()
     out, err = sh.communicate()
     if not sucess:
         if sh.returncode != errorCode:
             print("Wrong error code")
         if err != errOut:
             print("Wrong error output")
-        if err.decode('utf-8') == errOut and sh.returncode == errorCode and not sucess:
+        if err == errOut and sh.returncode == errorCode and not sucess:
             print("Passed")
     if sucess:
+        if stdOut == b"":
+            res = subprocess.Popen(command.split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            resOut, resErr = res.communicate()
+        else:
+            resOut = stdOut
         if sh.returncode != successCode:
             print("Wrong error code")
-        if err != errOut:
-            print("Wrong error output")
-        if err.decode('utf-8') == errOut and sh.returncode == errorCode and not sucess:
+        if resOut != out:
+            print("Wrong standard output")
+        if out == resOut and sh.returncode == successCode and sucess:
             print("Passed")
 
 
-subprocess.run(['make'])
-print("minishell1 tester")
-TEST("./seg", False, segfaultMessage, noErr)
-TEST("ls", True, noErr, noErr)
-TEST("exitt", False, commandNotFound, noErr)
+def main():
+    subprocess.run(['make', 're', '-s'])
+    print("minishell1 tester")
+    TEST("./seg", False, segfaultMessage, noOut)
+    TEST("ls", True, noErr, noOut)
+    TEST("exitt", False, commandNotFound, noOut)
+    TEST("echo test", True, noErr, b"test\n")
+    TEST("env", True, noErr, noOut)
+    subprocess.run(['make', 'clean', '-s'])
+
+
+if __name__ == "__main__":
+    main()
